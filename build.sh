79c39f4334e7a1f44540f8dbc408b55cc5e8c0c4
Paul Swenson <pdscomp@gmail.com>
2025-07-06 04:38:08 -0400

    Fixing build.sh typos

--- build.sh
+++ build.sh
@@ -4,13 +4,13 @@
 #
 
 echo "Unpacking the firmware..."
-sudo ./rc.unpack FW/FW-CentauriCarbon-v1.1.25-2025-05-09.bin
+sudo ./unpack.sh FW/FW-CentauriCarbon-v1.1.25-2025-05-09.bin
 echo
 
 echo "Patching the firmware..."
-sudo ./rc.patch
+sudo ./patch.sh
 echo
 
 echo "Re-packing the firmware into update/update.swu..."
-sudo ./rc.pack
+sudo ./pack.sh
 echo
