84b4f487ca013818e3a0e58829ebb094c6f999cf
suchmememanyskill <38142618+suchmememanyskill@users.noreply.github.com>
2025-10-14 14:48:14 +0200

    Set firmware version patch

---
+++ oc-patches/set-firmware-version-patch/patch.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+import subprocess, os
+
+SQUASHFS_ROOT = os.getenv("SQUASHFS_ROOT")
+REPOSITORY_ROOT = os.getenv("REPOSITORY_ROOT")
+
+def extract_commit() -> str:
+    git_describe_output = subprocess.run(["git", "--git-dir", os.path.join(REPOSITORY_ROOT, ".git"), "describe", "--tags"], stdout=subprocess.PIPE, text=True, check=True).stdout.strip()
+    split_output = git_describe_output.split("-")
+
+    if (len(split_output) >= 3):
+        version = f"{split_output[0]} ({split_output[2][1:]})"
+    else:
+        version = split_output[0]
+
+    if version.startswith("v"):
+        return version[1:]
+    
+    return version
+
+try:
+    version = extract_commit()
+except:
+    version = "Unknown"
+
+version = version + " OC\0"
+encoded = version.encode(encoding="ASCII")
+print(version)
+
+with open(os.path.join(SQUASHFS_ROOT, "app", "app"), "r+b") as fp:
+    fp.seek(0x34F6E8, os.SEEK_SET)
+    fp.write(encoded)
