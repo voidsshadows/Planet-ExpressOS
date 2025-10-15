bef634e71fddb686976b7cbed58387e958cbb286
Paul Swenson <pdscomp@gmail.com>
2025-10-15 10:06:31 -0400

    Adding patch notes for UART patch (breaks CC screen, but useful for kernel/uboot dev)

--- oc-patches/uart-patch/patch.sh
+++ oc-patches/uart-patch/patch.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Note: This patch currently disables the scree on the Elegoo CC making it un-usable for printing
+# We are working on an updated uBoot binary that will enable UART and also allow screen use!
+# For now this is only useful for devs doing DSP or Kernel development.
+
 set -e
 cd "$REPOSITORY_ROOT"
 OPTIONS_DIR="./RESOURCES/OPTIONS" ./RESOURCES/OPTIONS/uart/uart.sh . 2.3.9
