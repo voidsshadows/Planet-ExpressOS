a1b246c0d9cdcbc3c1181fa0381da9897f33dbe7
Michael Garcia <75304648+voidsshadows@users.noreply.github.com>
2025-10-20 14:01:19 -0500

    Update patch.sh

--- oc-patches/exploding-centauri/patch.sh
+++ oc-patches/exploding-centauri/patch.sh
@@ -1,1 +1,12 @@
+#!/usr/bin/env bash
+if [ $UID -ne 0 ]; then
+  echo "Error: Please run as root."
+  exit 1
+fi
 
+set -e
+
+cd "$EXTRACTED_FW_ROOT"
+
+# Replace old image with exploding.png renamed to 1
+cp -f "$EXTRACTED_FW_ROOT/images/exploding.png" "$EXTRACTED_FW_ROOT/app/resources/images/1"
